c0e98f3309df33d6c6c982c3b6d8521699936284
whiterook6 <whiterook6@gmail.com>
2017-03-11 12:40:58 -0800

    Added ELO Ranking
    #3

---
+++ src/main/java/faceoff/elo/ELO.java
@@ -0,0 +1,69 @@
+package faceoff.elo;
+
+public class ELO {
+
+	public double championScore;
+	private double championR;
+	private double championE;
+	public double challengerScore;
+	private double challengerR;
+	private double challengerE;
+
+	public static final double K = 32.0;
+
+	public ELO(double championScore, double challengerScore) {
+		this.championScore = championScore;
+		this.challengerScore = challengerScore;
+		
+		this.championR = Math.pow(10, (this.championScore / 400.0));
+		this.challengerR = Math.pow(10, (this.challengerScore / 400.0));
+		
+		double divisor = this.championR + this.challengerR;
+		this.championE = this.championR / divisor;
+		this.challengerE = this.challengerR / divisor;
+	}
+
+	public double getChampionScore() {
+		return championScore;
+	}
+	
+	public double getChampionR(){
+		return championR;
+	}
+	
+	public double getChampionE() {
+		return championE;
+	}
+
+	public double getChallengerScore() {
+		return challengerScore;
+	}
+	
+	public double getChallengerR(){
+		return challengerR;
+	}
+
+	public double getChallengerE() {
+		return challengerE;
+	}
+
+	public void championWins() {
+		championScore = championScore + (ELO.K * (1 - championE));
+		challengerScore = challengerScore + (ELO.K * (-challengerE));
+	}
+
+	public void challengerWins() {
+		championScore = championScore + (ELO.K * (-championE));
+		challengerScore = challengerScore + (ELO.K * (1 - challengerE));
+	}
+
+	public void bothWin() {
+		championScore = championScore + (ELO.K * (1 - championE));
+		challengerScore = challengerScore + (ELO.K * (1 - challengerE));
+	}
+
+	public void bothLose() {
+		championScore = championScore + (ELO.K * (-championE));
+		challengerScore = challengerScore + (ELO.K * (-challengerE));
+	}
+}
---
+++ src/test/java/embiggener/EloTest.java
@@ -0,0 +1,39 @@
+package embiggener;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import faceoff.elo.ELO;
+
+public class EloTest {
+
+	@Test
+	public void test_champion_wins() {
+		ELO elo = new ELO(2400, 2000);
+		elo.championWins();
+		assertEquals(2403, elo.getChampionScore(), 0.5);
+		assertEquals(1997, elo.getChallengerScore(), 0.5);
+	}
+	
+	@Test
+	public void test_challenger_wins() {
+		ELO elo = new ELO(2400, 2000);
+		elo.challengerWins();
+		assertEquals(2371, elo.getChampionScore(), 0.5);
+		assertEquals(2029, elo.getChallengerScore(), 0.5);
+	}
+	@Test
+	public void test_both_win() {
+		ELO elo = new ELO(2400, 2000);
+		elo.bothWin();
+		assertEquals(2403, elo.getChampionScore(), 0.5);
+		assertEquals(2029, elo.getChallengerScore(), 0.5);
+	}
+	
+	@Test
+	public void test_both_lose() {
+		ELO elo = new ELO(2400, 2000);
+		elo.bothLose();
+		assertEquals(2371, elo.getChampionScore(), 0.5);
+		assertEquals(1997, elo.getChallengerScore(), 0.5);
+	}
+}
